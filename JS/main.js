0ed29e7cc162a3c32c78e90b305daf519ddb29d3
1906548310@qq.com <1906548310@qq.com>
2016-10-18 15:21:47 +0800

    add response to navbar

--- JS/main.js
+++ JS/main.js
@@ -13,4 +13,23 @@ $(document).ready(function() {
 		//接着将active样式添加到所点击的这个元素上
 		$(this).addClass("active");
 	});
+	$('#myNavbar > ul > li:first-child').click(function() {
+		$('.mask_index div').css('display','none');
+	});
+	$('#myNavbar > ul > li:nth-child(2)').click(function() {
+		$('#blog_center').show();
+		$('#work_center').css('display','none');
+		$('#resume_center').css('display','none');
+	});
+	$('#myNavbar > ul > li:nth-child(3)').click(function() {
+		$('#work_center').show();
+		$('#blog_center').css('display','none');
+		$('#resume_center').css('display','none');
+	});
+	$('#myNavbar > ul > li:nth-child(4)').click(function() {
+		$('#resume_center').show();
+		$('#blog_center').css('display','none');
+		$('#work_center').css('display','none');
+	});
 });
+
